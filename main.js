edface60de1994b2f14ba7463c49926936e43bfd
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-10-31 13:41:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 object_status="";
-
+objects=[];
 
 
 
@@ -9,45 +9,49 @@ function preload(){
 
 
 function setup(){
-  canvas=createCanvas(965, 544);
-  canvas.position(180, 90);
-  object_detector=ml5.objectDetector("cocossd", modelLoaded);
-  document.getElementById("status").innerHTML="Status: Detecting Object";
+  canvas=createCanvas(500, 400);
+  canvas.center();
+  video=createCapture(VIDEO);
+  video.size(500,400)
+  video.hide();
+  objectDetector=ml5.objectDetector("cocosst", modelLoaded);
 }
 
 
 function draw(){
-    image(img, 0,  0, 965, 544);
-    fill("red");
-    text("Dog", 130,130);
-    noFill();
-    stroke("red");
-    rect(120, 110, 700,400);
+    image(video, 0,  0, 500, 400);
+        if(object_status != ""){  
+          object_detector.detect(video, gotRuselt);
+           var r= random(255);
+           var g=random(255);
+           var b=random(255);
+           for(var i;  objects.length < i; i++){
+           document.getElementById("status").innerHTML="Status: Detecting Object";
+           fill(r,g,b);
+           percent=Math.floor(objects[i].confidence*100);
+           text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y);
+           noFill();
+           stroke(r,g,b);
+           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+           }
 
-    noFill();
-    noStroke();
-
-    fill("yellow");
-    text("Cat", 440,150);
-    noFill();
-    stroke("yellow");
-    rect(430 ,130, 400,350);
-    noStroke();
-}
+            
+           }
+      }   
+    }
 
 
 function modelLoaded(){
-    console.log("Model is loaded......");
-    object_status=true;
-    object_detector.detect(img, gotRuselt);
+  object_status=true;
+  object_detector.detect(video, gotRuselt);
 }
 
 
-function gotRuselt(error ,results){
+function gotRuselt(error,ruselts){
   if(error){
     console.error(error);
   }
   else{
-    console.log(results);
+    console.log(ruselts);
   }
 }
