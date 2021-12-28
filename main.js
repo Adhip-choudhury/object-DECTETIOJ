f649c1530f98f2ee6e6439e6b6e08068350066a0
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-12-28 19:06:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,44 +14,47 @@ function setup(){
   video=createCapture(VIDEO);
   video.size(500,400)
   video.hide();
-  objectDetector=ml5.objectDetector("cocosst", modelLoaded);
 }
 
 
 function draw(){
     image(video, 0,  0, 500, 400);
-        if(object_status != ""){  
-          object_detector.detect(video, gotRuselt);
-           var r= random(255);
-           var g=random(255);
-           var b=random(255);
-           for(var i;  objects.length < i; i++){
-           document.getElementById("status").innerHTML="Status: Detecting Object";
-           fill(r,g,b);
-           percent=Math.floor(objects[i].confidence*100);
-           text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y);
-           noFill();
-           stroke(r,g,b);
-           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-           }
+    if(object_status != ""){
+      objectDetector.detect(video, gotRuselt); 
+      for(var i = 0; i <= objects.length; i++){
+      document.getElementById("status").innerHTML="Status: detected objects";
+      document.getElementById("Number_of_objects").innerHTML="Number of objects are there is "+objects.length;
+      // fill("red");
+      // text(objects[i].label, objects[i].x+15, objects[i].y+10);
+      fill("#FF0000"); 
+      percent = floor(objects[i].confidence * 100); 
+      text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+      }
+    }     
+  }
 
-            
-           }
-      }   
+      
+    
+
+      function modelLoaded(){
+        console.log("Model is loaded......");
+        object_status=true;
+    }
+    
+
+    function start(){
+        objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+      document.getElementById("status").innerHTML="Status: detecting objects";
     }
 
 
-function modelLoaded(){
-  object_status=true;
-  object_detector.detect(video, gotRuselt);
-}
 
-
-function gotRuselt(error,ruselts){
-  if(error){
-    console.error(error);
-  }
-  else{
-    console.log(ruselts);
-  }
-}
+    function gotRuselt(error, results){
+      if(error){
+        console.error(error);
+      }
+      else{
+        console.log(results);
+        objects=results;
+      }
+    }
